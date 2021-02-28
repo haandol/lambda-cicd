a4f53986044f9713c8c258841f55acba34697799
Dongkyun Lee <dongkyl@amazon.com>
2021-03-01 01:48:55 +0900

    secret

--- infra/lib/stacks/pipeline-stack.ts
+++ infra/lib/stacks/pipeline-stack.ts
@@ -76,7 +76,7 @@ export class PipelineStack extends cdk.Stack {
           actions: [
             new codepipeline_actions.GitHubSourceAction({
               actionName: 'CodeCommit_Source',
-              oauthToken: cdk.SecretValue.ssmSecure('github-token', '3'),
+              oauthToken: cdk.SecretValue.secretsManager('github-token'),
               owner: 'haandol',
               repo: 'lambda-cicd-tutorial',
               branch: 'main',
